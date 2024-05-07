3afdd58227c059810b1517a77ba57e83a5d2d099
lkzc19 <lkzc19@foxmail.com>
2024-05-07 08:59:47 +0800

    feat: map Keys & Values

---
+++ mapx/mapx.go
@@ -0,0 +1,19 @@
+package mapx
+
+// Keys 获取map的key
+func Keys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// Values 获取map的value
+func Values[K comparable, V any](m map[K]V) []V {
+	values := make([]V, 0, len(m))
+	for _, v := range m {
+		values = append(values, v)
+	}
+	return values
+}
