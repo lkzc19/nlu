24206453dbff6073757d43656640b6fe8b90b837
lkzc19 <lkzc19@foxmail.com>
2024-07-30 15:41:45 +0800

    test: timex IsCurrent

--- timex/timex.go
+++ timex/timex.go
@@ -4,13 +4,17 @@ import (
 	"time"
 )
 
+func IsCurrentDay(t time.Time) bool {
+	year, month, day := time.Now().Date()
+	return t.Year() == year && t.Month() == month && t.Day() == day
+}
+
 func IsCurrentMonth(t time.Time) bool {
-	now := time.Now()
-	year, month, _ := now.Date()
-	// 获取当月的第一天
-	firstDay := time.Date(year, month, 1, 0, 0, 0, 0, now.Location())
-	// 获取当月的最后一天
-	lastDay := firstDay.AddDate(0, 1, -1)
-	// 判断当前时间是否在当月
-	return t.After(firstDay) && t.Before(lastDay)
+	year, month, _ := time.Now().Date()
+	return t.Year() == year && t.Month() == month
+}
+
+func IsCurrentYear(t time.Time) bool {
+	year, _, _ := time.Now().Date()
+	return t.Year() == year
 }
--- timex/timex_test.go
+++ timex/timex_test.go
@@ -6,10 +6,32 @@ import (
 	"time"
 )
 
+func TestIsCurrentDay(t *testing.T) {
+	fmt.Println(IsCurrentDay(time.Now()))
+
+	fmt.Println(IsCurrentDay(time.Now().AddDate(1, 0, 0)))
+
+	fmt.Println(IsCurrentDay(time.Now().AddDate(0, 1, 0)))
+
+	fmt.Println(IsCurrentDay(time.Now().AddDate(0, 0, 1)))
+}
+
 func TestIsCurrentMonth(t *testing.T) {
 	fmt.Println(IsCurrentMonth(time.Now()))
 
 	fmt.Println(IsCurrentMonth(time.Now().AddDate(1, 0, 0)))
 
 	fmt.Println(IsCurrentMonth(time.Now().AddDate(0, 1, 0)))
+
+	fmt.Println(IsCurrentMonth(time.Now().AddDate(0, 1, 1)))
+}
+
+func TestIsCurrentYear(t *testing.T) {
+	fmt.Println(IsCurrentYear(time.Now()))
+
+	fmt.Println(IsCurrentYear(time.Now().AddDate(1, 0, 0)))
+
+	fmt.Println(IsCurrentYear(time.Now().AddDate(0, 1, 0)))
+
+	fmt.Println(IsCurrentYear(time.Now().AddDate(0, 0, 1)))
 }
